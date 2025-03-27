f0ba1c0024416edfc271ecd1cfac533dd32aca15
Solomon Ador-Dionisio <dionis17@ads.utm.utoronto.ca>
2025-03-27 10:40:09 -0400

    yuh

---
+++ js/navbar.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function() {
+  let navbar = document.getElementsByTagName("nav")[0];
+  let creditstab = document.getElementsByClassName("popup")[0];
+  let blackcover = document.getElementsByClassName("cover")[0];
+
+  let authortab = document.getElementsByClassName("popup")[1];
+
+  let returnicons = document.getElementsByClassName("return");
+
+
+  blackcover.addEventListener("click", function (e) {
+    blackcover.style.zIndex = "-1";
+    creditstab.style.zIndex = "-1";
+    authortab.style.zIndex = "-1";
+  });
+
+
+  navbar.children[1].addEventListener("click", function (e) {
+    blackcover.style.zIndex = "2";
+    creditstab.style.zIndex = "3";
+  });
+
+  navbar.children[2].addEventListener("click", function (e) {
+    blackcover.style.zIndex = "2";
+    authortab.style.zIndex = "3";
+  });
+
+
+  for (let i = 0; i < returnicons.length; i++) {
+    returnicons[i].addEventListener("click", function(e) {
+      blackcover.style.zIndex = "-1";
+      creditstab.style.zIndex = "-1";
+      authortab.style.zIndex = "-1";
+    });
+  }
+});
