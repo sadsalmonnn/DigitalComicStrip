27b7e32cc3e36e618de47d657182093b34ac6473
dionis17 <solomon.adordionisio@mail.utoronto.ca>
2025-03-29 23:15:07 -0400

    fdhjsffhjsdf

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,5 @@
-window.addEventListener('DOMContentLoaded', function() {
+
+window.addEventListener('DOMContentLoaded', function () {
   let navbar = document.getElementsByTagName("nav")[0];
   let creditstab = document.getElementsByClassName("popup")[0];
   let blackcover = document.getElementsByClassName("cover")[0];
@@ -12,25 +13,33 @@ window.addEventListener('DOMContentLoaded', function() {
     blackcover.style.zIndex = "-1";
     creditstab.style.zIndex = "-1";
     authortab.style.zIndex = "-1";
+    creditstab.style.opacity = "0";
+    authortab.style.opacity = "0";
   });
 
 
   navbar.children[1].addEventListener("click", function (e) {
     blackcover.style.zIndex = "2";
-    creditstab.style.zIndex = "3";
+    creditstab.style.zIndex = "4";
+    creditstab.style.opacity = "1";
+    authortab.style.opacity = "1";
   });
 
   navbar.children[2].addEventListener("click", function (e) {
     blackcover.style.zIndex = "2";
-    authortab.style.zIndex = "3";
+    authortab.style.zIndex = "4";
+    creditstab.style.opacity = "1";
+    authortab.style.opacity = "1";
   });
 
 
   for (let i = 0; i < returnicons.length; i++) {
-    returnicons[i].addEventListener("click", function(e) {
+    returnicons[i].addEventListener("click", function (e) {
       blackcover.style.zIndex = "-1";
       creditstab.style.zIndex = "-1";
       authortab.style.zIndex = "-1";
+      creditstab.style.opacity = "0";
+      authortab.style.opacity = "0";
     });
   }
 });
